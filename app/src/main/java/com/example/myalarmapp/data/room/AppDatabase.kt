aa7ce76f6e353e8993c1eeda680f9010b9979e01
ar9988 <rladkfma9988@gmail.com>
2024-12-14 19:11:59 +0900

    .

--- app/src/main/java/com/example/myalarmapp/data/room/AppDatabase.kt
+++ app/src/main/java/com/example/myalarmapp/data/room/AppDatabase.kt
@@ -1,8 +1,9 @@
 package com.example.myalarmapp.data.room
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 
 @Database(entities = [AlarmEntity::class], version = 1)
-abstract class AppDatabase {
+abstract class AppDatabase : RoomDatabase() {
     abstract fun alarmDao():AlarmDao
 }
