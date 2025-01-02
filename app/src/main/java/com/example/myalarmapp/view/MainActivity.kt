a42ded5182afd61ff1c52c6173ae927e81cf9b28
ar9988 <rladkfma9988@gmail.com>
2025-01-03 00:58:54 +0900

    .

--- app/src/main/java/com/example/myalarmapp/view/MainActivity.kt
+++ app/src/main/java/com/example/myalarmapp/view/MainActivity.kt
@@ -8,16 +8,24 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -25,6 +33,10 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.myalarmapp.R
 import com.example.myalarmapp.view.theme.MyAlarmAppTheme
 
@@ -47,8 +59,22 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApp(name: String, modifier: Modifier = Modifier) {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "home") {
+        composable("home") {
+            HomeScreen(navController)
+        }
+        composable("addAlarm") {
+            AddAlarmScreen(navController)
+        }
+    }
+
+}
+
+@Composable
+fun HomeScreen(navController: NavController) {
     val items = listOf("Item 1", "Item 2", "Item 3", "Item 4", "Item 5")
-    Column(modifier = modifier) {
+    Column(modifier = Modifier) {
         Box(
             modifier = Modifier
                 .fillMaxHeight(0.3f)
@@ -72,13 +98,13 @@ fun MyApp(name: String, modifier: Modifier = Modifier) {
             verticalArrangement = Arrangement.Bottom,
             horizontalAlignment = Alignment.End
         ) {
-            Button(onClick = { /*TODO*/ }) {
+            Button(onClick = { navController.navigate("addAlarm") }) {
                 Text(text = "+")
             }
         }
         LazyColumn(
             modifier = Modifier
-                .fillMaxHeight(0.5f) // 전체 화면의 절반만 차지
+                .fillMaxHeight(0.5f)
                 .padding(16.dp)
         ) {
             items(items) { item ->
@@ -87,6 +113,29 @@ fun MyApp(name: String, modifier: Modifier = Modifier) {
         }
     }
 }
+
+@Composable
+fun AddAlarmScreen(navController: NavController) {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Set an Alarm", style = MaterialTheme.typography.headlineLarge)
+
+
+        Button(
+            onClick = {
+                navController.popBackStack()
+            }
+        ) {
+            Text("Add Alarm")
+        }
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
